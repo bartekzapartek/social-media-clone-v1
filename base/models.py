4b9288dda6b399f601c70d3d0d27ce1885139fc7
Bartłomiej Stefański <bartlomiej23472@gmail.com>
2023-10-21 11:22:52 +0200

    Add basic model layer, Add basic navbar styling

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    owner = models.OneToOneField(User, on_delete = models.CASCADE)
+    likes = models.ManyToManyField(User, related_name = 'users_likes')
+
+    description = models.TextField()
+
+    created = models.DateTimeField(auto_now = True)
+    updated = models.DateTimeField(auto_now_add = True)
+
+
+class Comment(models.Model):
+    owner = models.OneToOneField(User, on_delete = models.CASCADE)
+    post = models.OneToOneField(Post, on_delete = models.CASCADE)
+
+    body = models.TextField(blank = False, null = False)
+
+    created = models.DateTimeField(auto_now = True)
+    updated = models.DateTimeField(auto_now_add = True)
