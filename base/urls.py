43d05e9a1d252b33f3cdbba44614a61e9998288d
Bartłomiej Stefański <bartlomiej23472@gmail.com>
2023-10-21 23:45:20 +0200

    Working auth. funct., Add post creation funct., Add basic home page funct., Develop design

--- base/urls.py
+++ base/urls.py
@@ -5,9 +5,11 @@ from . import views
 urlpatterns = [
 
     path('', views.home_view, name = 'home'),
-    path('user-profile/<str:pk>/', views.user_profile_view, name = 'user-profile'),
+    path('user-profile/<str:username>/', views.user_profile_view, name = 'user-profile'),
     path('post/<str:pk>/', views.post_view, name = 'post'),
-    path('explore/', views.explore_view, name = 'explore')
+    path('explore/', views.explore_view, name = 'explore'),
+
+    path('create/', views.create_view, name = 'create')
 
 
 ]
--- base/views.py
+++ base/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Post, Comment, UserProfile
+from .forms import PostForm
+
 from django.contrib.auth.decorators import login_required
 
 
 
 @login_required(login_url = 'login')
 def home_view(request):
-    #logged_user = UserProfile.objects.get(user = request.user)
+    logged_user = UserProfile.objects.get(user = request.user)
+    following_users = logged_user.following.all()
+    following_posts = []
 
-    context = {}
+    for user in following_users:
+        following_posts += Post.objects.filter(owner = user) 
+
+    print(following_posts)
+    
+    context = {'following_posts' : following_posts} 
     return render(request, 'base/home.html', context)
 
 
@@ -24,7 +33,28 @@ def post_view(request, pk):
     return render(request, 'base/post.html', context)
 
 
-def user_profile_view(request, pk):
+def user_profile_view(request, username):
 
     context = {}
     return render(request, 'base/home.html', context)
+
+
+@login_required(login_url = 'login')
+def create_view(request):
+    post_form = PostForm()
+
+    if request.method == 'POST':
+        post_form = PostForm(request.POST)
+
+        if post_form.is_valid():
+            post = post_form.save(commit = False)
+            
+            post.owner = request.user
+
+            post.save()
+
+            return redirect('home')
+
+
+    context = {'post_form' : post_form}
+    return render(request, 'base/create.html', context)
